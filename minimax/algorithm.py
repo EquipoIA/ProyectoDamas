0d3d281ac550d20d68a677edd40037f69ad05f15
aperelman21 <55751794+aperelman21@users.noreply.github.com>
2021-04-18 13:45:48 -0500

    agregamos comentarios

--- minimax/algorithm.py
+++ minimax/algorithm.py
@@ -53,47 +53,41 @@ def minimaxRed(position, depth, max_player,alpha,beta, game):
                 return beta,best_move   
         return beta, best_move
 
-def minimaxBlue(position, depth, max_player,alpha,beta, game):
-    if depth == 0 or position.winner() != None:
+#Algoritmo minimax con alpha beta
+def minimaxBlue(position, depth, max_player,alpha,beta):
+    if depth == 0 or position.winner() != None: #si ya bajamos al ultimo nivel permitido o ya no tiene hijos
         return heuristica(position, BLUE), position
     
-    if max_player:
+    if max_player:#si estamos buscando maximizar el valor
         best_move = None
-        for move in get_all_moves(position, BLUE, game):
-            evaluation = minimaxBlue(move, depth-1, False,alpha, beta, game)[0]
-            if evaluation > alpha:
+        for move in get_all_moves(position, BLUE): #recorre los hijos del nodo en el que estamos
+            evaluation = minimaxBlue(move, depth-1, False,alpha, beta)[0]
+            if evaluation > alpha:#si el valor del hijo es mayor a alpha, se hace el cambio
                 alpha = evaluation
                 best_move = move
-            if alpha >= beta:
+            if alpha >= beta: #parte que hace el "pruning" de las ramas
                 return alpha, best_move  
         return alpha,best_move
-    else:
+    else: #si estamos buscanod minimizar el valor
         best_move = None
-        for move in get_all_moves(position, RED, game):
-            evaluation = minimaxBlue(move, depth-1, True,alpha,beta, game)[0]
-            if evaluation < beta:
+        for move in get_all_moves(position, RED):#recorre los hijos del nodo en el que estamos
+            evaluation = minimaxBlue(move, depth-1, True,alpha,beta)[0]
+            if evaluation < beta: #si el valor es menor a beta, se hace el cambio
                 beta = evaluation
                 best_move = move
-            if alpha >= beta:
+            if alpha >= beta:#parte que hace el "pruning" de las ramas
                 return beta,best_move   
         return beta, best_move
 
 
-def simulate_move(piece, move, board, game, skip):
+def simulate_move(piece, move, board, skip):
     board.move(piece, move[0], move[1])
     if skip:
         board.remove(skip)
 
     return board
 
-def simula_mov(piece,move,board,game,skip):
-    temp_tablero = deepcopy(board)
-    temp_tablero.move(piece, move[0], move[1])
-    if skip:
-        temp_tablero.remove(skip)
-    return temp_tablero
-
-def get_all_moves(board, color, game):
+def get_all_moves(board, color):
     moves = []
 
     for piece in board.get_all_pieces(color):
@@ -102,17 +96,7 @@ def get_all_moves(board, color, game):
             
             temp_board = deepcopy(board)
             temp_piece = temp_board.get_piece(piece.row, piece.col)
-            new_board = simulate_move(temp_piece, move, temp_board, game, skip)
+            new_board = simulate_move(temp_piece, move, temp_board,skip)
             moves.append(new_board)
-        #draw_moves(game, board, piece)
     return moves
-
-
-def draw_moves(game, board, piece):
-    valid_moves = board.get_valid_moves(piece)
-    board.draw(game.win)
-    pygame.draw.circle(game.win, (0,255,0), (piece.x, piece.y), 50, 5)
-    game.draw_valid_moves(valid_moves.keys())
-    pygame.display.update()
-    #pygame.time.delay(100)
 
