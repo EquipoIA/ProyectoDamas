964a3969507acb491609bd545c4da16442c53b80
guillermoNaranjo <70484786+guillermoNaranjo@users.noreply.github.com>
2021-05-07 13:16:20 -0500

    Avance con entropías

--- id3.py
+++ id3.py
@@ -16,57 +16,58 @@ class arbolDeClasificacion:
         self.nombres=nombres
         #self.categoriasC=list(set())
 
-    def entropia(self,columna):
-        cat=self.categorias(columna)
-        numInst=self.numInstancias(cat,columna)
+    def entropia(self,k):
         entropia=0
-        n,o=self.cuenta_iguales(columna)
-        for i in range(len(cat)):
-            entropia+= ((-1)*(numInst[i]/sum(numInst)))*((n[i]/numInst[i])*math.log((n[i]/numInst[i]), 2)+(o[i]/numInst[i])*math.log((o[i]/numInst[i]), 2))
+        cats=self.cat_total()
+        n,o=self.cuenta_iguales(k,cats)
+        #print("N y O")
+        #print(n)
+        #print(o)
+        for i in range(len(cats[k])):
+            if n[i]!=0 and o[i]!=0: 
+                entropia+= ((-1)*((n[i]+o[i])/100))*((n[i]/(n[i]+o[i]))*math.log((n[i]/(n[i]+o[i])), 2)+(o[i]/(n[i]+o[i]))*math.log((o[i]/(n[i]+o[i])), 2))
+            elif n[i]==0: 
+                entropia+= ((-1)*((n[i]+o[i])/100))*((o[i]/(n[i]+o[i]))*math.log((o[i]/(n[i]+o[i])), 2))
+            elif o[i]==0:
+                entropia+= ((-1)*((n[i]+o[i])/100))*((n[i]/(n[i]+o[i]))*math.log((n[i]/(n[i]+o[i])), 2))
         return entropia
     
-    def cuenta_iguales(self, columna): 
+    def cat_total(self):
+        cat_tot = []
+        for i in range(len(self.nombres)):
+            cat_tot.append(self.categorias(self.nombres[i]))
+        return cat_tot
+
+    def cuenta_iguales(self,k,cats): 
         catN=[]
         catO=[]
-        dt = self.x.loc[datos.Fertilidad == "O",:]
-        dt2 = self.x.loc[datos.Fertilidad == "N",:]
-        for i in range(len(categorias)):
-            n=len(dt.loc[dt.Edad == categorias[i],:])
+        dt = self.x.loc[self.x.Fertilidad == "O",:]
+        dt2 = self.x.loc[self.x.Fertilidad == "N",:]
+        for i in range(len(cats[k])):
+            n=len(dt.loc[dt.iloc[:,k]== cats[k][i],:])
             catN.append(n)
-            o=len(dt2.loc[dt2.Edad == categorias[i],:])
+            o=len(dt2.loc[dt2.iloc[:,k] == cats[k][i],:])
             catO.append(o)
         return catN, catO
 
-            
     def categorias(self,columna):
         categorias=[]
         for i in self.x[columna]: 
             if i not in categorias: 
                 categorias.append(i)        
         return categorias 
-
-    def numInstancias(self, categorias, columna): 
-        vecesIns=[]
-        for i in categorias: 
-            vecesIns.append(list(self.x[columna]).count(i))
-        return vecesIns
     
 os.chdir("C:\\Users\\memon\\Downloads")
-nombres=["Estación", "Edad", "Enfermedades", "Accidente", "Cirugía", "Fiebre", "Alcohol", "Fumador", "Inactivo", "Fertilidad"]
+nombres=["Estacion", "Edad", "Enfermedades", "Accidente", "Cirugia", "Fiebre", "Alcohol", "Fumador", "Inactivo", "Fertilidad"]
 datos=pd.read_csv('fertility_Diagnosis.txt', header=None, names=nombres)
-print(datos)
-arbol=arbolDeClasificacion(datos, nombres)      
-categorias=(arbol.categorias("Edad"))
-print(categorias)
-print("lel")
-num=arbol.numInstancias(categorias, "Edad")
-n,o=arbol.cuenta_iguales("Edad")
-print(n)
-print(o)
-print(sum(n)+sum(o))
-print(num)
-entropia=arbol.entropia("Edad")
-print(entropia)
+#print(datos)
+arbol=arbolDeClasificacion(datos, nombres)
+a=arbol.cat_total()
+n,o=arbol.cuenta_iguales(0,a)
+for i in range(len(a)-1):
+    print(arbol.entropia(i))
+
+
 
 
 
